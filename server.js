00aa5ab4db1f53e7836d31276459dd3d8d6f8ae0
MaryKoul <marykoul2@yahoo.com>
2025-04-28 22:49:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 app.use(bodyParser.json());
 
 app.post('/dialogflowGateway', async (req, res) => {
@@ -40,4 +43,4 @@ app.post('/dialogflowGateway', async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-});
+}); 
